a6c5bd155127b037afe5f83aea05d5ed93a6f91a
JeanSuw <119563534+JeanSuw@users.noreply.github.com>
2023-04-07 22:23:36 -0700

    Added login and out js file for the user

--- public/js/login.js
+++ public/js/login.js
@@ -1,2 +1,23 @@
 // Things that user needs to login
 // username and password
+const loginFormHandler = async (event) => {
+    event.preventDefault();
+    const email = document.querySelector('#email-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+  
+    if (email && password) {
+      const response = await fetch('/api/users/login', {
+        method: 'POST',
+        body: JSON.stringify({ email, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/profile');
+      } else {
+        alert(response.statusText);
+      }
+    }
+};
+
+document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
